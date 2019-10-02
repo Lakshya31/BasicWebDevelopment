4023e8b3cddce607a2bdfb8991c623d9e3c42b21
Lakshya31 <lakshya.secret@gmail.com>
2019-10-02 14:40:18 +0530

    Tempo

--- SimpleToDoList/script.js
+++ SimpleToDoList/script.js
@@ -1,20 +1,70 @@
-ul = document.getElementById("TodoList")
-input = document.getElementById("inputbox")
-AddItem = document.getElementById("AddItem")
+var ul = document.getElementById("TodoList");
+var input = document.getElementById("inputbox");
+var AddItem = document.getElementById("AddItem");
+var List = document.getElementsByClassName("ToDoItem");
+var Save = document.getElementById("SaveList");
+
 
 var AddItemtoList = function(){
+    li = document.createElement("li");
+    li.classList.add("ToDoItem");
+    li.addEventListener("click",ClickDelete);
+    value = document.createTextNode(input.value);
+    li.appendChild(value);
+    ul.appendChild(li);
+};
+
+var ClickAdd = function(){
     if (input.value.length > 0){
-        li = document.createElement("li");
-        value = document.createTextNode(input.value)
-        li.appendChild(value)
-        ul.appendChild(li)
+        AddItemtoList();
+        input.value = "";
     }
+    
+};
+
+var EnterAdd= function(){
+    if (event.keyCode === 13 && input.value.length > 0){
+        AddItemtoList();
+        input.value = "";
+    }
+    
+};
+
+var ClickDelete = function(event){
+    li = event.path[0]
+    ul.removeChild(li)
 }
 
-AddItem.addEventListener("click",AddItemtoList())
-input.addEventListener("keypress", function(){
-    if (event.keyCode === 13){
-        AddItemtoList()
-    }
-})
+function DownloadList() {
 
+    var List = document.getElementsByClassName("ToDoItem");
+    var filename = "ToDoList.txt"
+    var text = ""
+
+    for (i=0; i<List.length; i++)
+    {
+        text = text + List[i].textContent
+        text = text + "\n"
+    }
+
+    console.log(text)
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+
+AddItem.addEventListener("click",ClickAdd);
+input.addEventListener("keypress", EnterAdd);
+Save.addEventListener("click",DownloadList);
+
+for(i=0; i<List.length; i++){
+    List[i].addEventListener("click",ClickDelete);
+}
